7776cd825a8d486fee26159677a52f7512b3ec3b
Miguigg <miguigg1412@gmail.com>
2022-11-23 18:50:53 +0100

    layout reset contraseña

---
+++ app/src/main/java/com/tfg/mifeed/resetContrasenha.java
@@ -0,0 +1,14 @@
+package com.tfg.mifeed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class resetContrasenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_contrasenha);
+    }
+}
