3c3cad5115719ffac4f6bf253c8d5d8edc92966f
Miguigg <miguigg1412@gmail.com>
2023-04-07 16:57:32 +0200

    Mejoras en API
    -Mejoras en las peticiones a NewsAPI

--- app/src/main/java/com/tfg/mifeed/controlador/conexionNewsApi/ApiConnInterface.java
+++ app/src/main/java/com/tfg/mifeed/controlador/conexionNewsApi/ApiConnInterface.java
@@ -9,13 +9,21 @@ import retrofit2.http.Query;
 public interface ApiConnInterface {
     String BASE_URL = "https://newsapi.org/v2/";
     //top-headlines?language=es&sortBy=publishedAt
-    @GET("top-headlines?sortBy=publishedAt")
+   /* @GET("top-headlines?sortBy=publishedAt")
     Call<RespuestaListaNoticias> getNoticiasPorTema(
             @Query(value = "country", encoded = true) String country,
             @Query("pageSize") int pageSize,
             @Query(value = "category",encoded = true) String category ,
             @Query("apiKey") String apiKey
+    );*/
+
+    @GET("everything?language=es&sortBy=publishedAt")
+    Call<RespuestaListaNoticias> getNoticiasPorTema(
+            @Query("pageSize") int pageSize,
+            @Query(value = "q",encoded = true) String category ,
+            @Query("apiKey") String apiKey
     );
+
 
     @GET("everything?language=es&sortBy=publishedAt")
     Call<RespuestaListaNoticias> getNoticiasImportantes(
